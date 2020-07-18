a57696f9d9a66656e485b3ce5b43b7374e35a827
FC <wcoffee995@gmail.com>
2020-07-18 16:15:33 +0800

    Initial Commit

---
+++ MimicHealthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MimicHealthApp
+//
+//  Created by Freda Cheng on 2020/7/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
